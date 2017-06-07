5781d3923b306f27f584bc79e372615ccd7bc904
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-06-07 12:56:35 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  function wait(ms) {
+    return new Promise(r => setTimeout(r, ms)).then(() => "Yay");
+  }
+
+  // do some work in background.
+  setInterval(() => 42, 200);
+
+  async function test() {
+    debugger;
+    const hello = "world";
+    const response = await fetch('index.html');
+    const tmp = await wait(1000);
+    console.log(tmp);
+    return hello;
+  }
+
+  async function runTest() {
+    let result = await test();
+    console.log(result);
+  }
+
+  runTest();
+})();
